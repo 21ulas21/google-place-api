8d884a157ba0180ba5f72afcddcca4648166fcab
ulasegrik <Ulasegrk21@hotmail.com>
2023-06-20 16:30:40 +0300

    proje olusturuldu

---
+++ google-places-api/src/main/java/com/egrikulas/googleplacesapi/GooglePlacesApiApplication.java
@@ -0,0 +1,13 @@
+package com.egrikulas.googleplacesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglePlacesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglePlacesApiApplication.class, args);
+	}
+
+}
---
+++ google-places-api/src/test/java/com/egrikulas/googleplacesapi/GooglePlacesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egrikulas.googleplacesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglePlacesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
